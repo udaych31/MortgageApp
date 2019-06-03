6e9200fe01c201df5fc6c4ca6140b723baef425b
user1 <user1@LP-5GXW8R1.HCLT.CORP.HCL.IN>
2019-06-03 14:12:25 +0530

    ismail

--- src/main/java/com/hcl/mortgage/app/controller/LoanApplicationController.java
+++ src/main/java/com/hcl/mortgage/app/controller/LoanApplicationController.java
@@ -1,10 +1,27 @@
 package com.hcl.mortgage.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.mortgage.app.dto.RequestPojo;
+import com.hcl.mortgage.app.dto.createResponse;
+import com.hcl.mortgage.app.service.LoanApplicationService;
 
 @RestController
 @CrossOrigin
 public class LoanApplicationController {
 
+	@Autowired
+	LoanApplicationService loanApplicationService;
+	@PostMapping("/createapplication")
+	public ResponseEntity<createResponse> createapplication(@RequestBody RequestPojo requestPojo)
+	{
+		 ResponseEntity<createResponse> res=loanApplicationService.createapplication(requestPojo);
+		 return res;
+		
+	}
 }
--- src/main/java/com/hcl/mortgage/app/service/LoanApplicationService.java
+++ src/main/java/com/hcl/mortgage/app/service/LoanApplicationService.java
@@ -1,5 +1,12 @@
 package com.hcl.mortgage.app.service;
 
+import org.springframework.http.ResponseEntity;
+
+import com.hcl.mortgage.app.dto.RequestPojo;
+import com.hcl.mortgage.app.dto.createResponse;
+
 public interface LoanApplicationService {
+
+	ResponseEntity<createResponse> createapplication(RequestPojo requestPojo);
 
 }
